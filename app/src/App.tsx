847fe649e27697a9ac2e5937b4dc2617c60fe94a
Naohiro Ishihara <nishi@NaohironoMacBook-Pro.local>
2022-01-03 21:55:45 +0900

    クエリフックオプション、戻り値の学習

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,9 +5,47 @@ import { useGetPokemonByNameQuery } from './services/pokemon';
 import './App.css';
 
 function App() {
-  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur');
-  if (error) return <div>Oh no, there was an error</div>;
-  if (isLoading) return <div>Loading...</div>;
+  // データフェッチ(フシギダネ)
+  const { data, error, isError, isUninitialized, isLoading, isFetching, refetch } = useGetPokemonByNameQuery(
+    'bulbasaur',              // クエリパラメータ
+    {
+      skip: false,            // trueにすると実行しない(実行させないためのフラグとして使用できる)
+      pollingInterval: 30000, // ポーリング間隔(ミリ秒)
+    }
+  );
+  // データフェッチ(ピカチュウ)
+  const { name, url } = useGetPokemonByNameQuery(
+    'pikachu',
+    {
+      // 取得後にデータ整形
+      selectFromResult: ({ data }) => ({ name: data?.species.name, url: data?.sprites.front_shiny }),
+      refetchOnMountOrArgChange: false,  // マウント時の再フェッチ指定
+      refetchOnFocus: true,              // ウインドウへフォーカスが戻った時の再フェッチ指定(setupListenersで呼び出されていないと使用不可)
+      refetchOnReconnect: true,          // ネットワーク接続が回復した時の再フェッチ指定(setupListenersで呼び出されていないと使用不可)
+    }
+  );
+
+  // エラー時
+  if (isError) return <div>Oh no, there was an error {error}</div>;
+
+  // 実行前
+  if (isUninitialized) {
+    console.log('Uninitialized');
+    return <div>Uninitialized...</div>;
+  }
+
+  // 処理中(初回のみ)
+  if (isLoading) {
+    console.log('Loading');
+    return <div>Loading...</div>;
+  };
+
+  // 処理中(ポーリングごと)
+  if (isFetching) {
+    console.log('ReFetching');
+    return <div>ReFetching...</div>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -54,8 +92,12 @@ function App() {
             React Redux
           </a>
         </span>
-        {data && (<img src={data.sprites.front_shiny} alt={data.species.name} />)}
       </header>
+      <div>
+        <button onClick={() => refetch()}>フシギダネrefetch</button>
+      </div>
+      {data && (<img src={data.sprites.front_shiny} alt={data.species.name} />)}
+      <img src={url} alt={name} />
     </div>
   );
 }
