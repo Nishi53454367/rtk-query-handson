cf2b8b9027fc9b7c71b47bbbf2520a250bd3fbc6
Naohiro Ishihara <nishi@NaohironoMacBook-Pro.local>
2022-02-16 22:50:38 +0900

    refetchOnFocusとrefetchOnReconnectの有効化

--- app/src/Pikachu.tsx
+++ app/src/Pikachu.tsx
@@ -11,8 +11,8 @@ const Pikachu = () => {
       // selectFromResultで取得後に任意のデータに整形も可能
       selectFromResult: ({ data }) => ({ name: data?.species.name, url: data?.sprites.front_shiny }),
       refetchOnMountOrArgChange: 10,  // マウント時の再フェッチ指定。数字指定(秒数)の場合、指定された時間の間はキャッシュを使用する。
-      refetchOnFocus: true,           // ウインドウへフォーカスが戻った時の再フェッチ指定(setupListenersで呼び出されていないと使用不可)
-      refetchOnReconnect: true,       // ネットワーク接続が回復した時の再フェッチ指定(setupListenersで呼び出されていないと使用不可)
+      refetchOnFocus: true,           // ウインドウへフォーカスが戻った時の再フェッチ指定(setupListenersがstoreで設定されてないと使用不可)
+      refetchOnReconnect: true,       // ネットワーク接続が回復した時の再フェッチ指定(setupListenersがstoreで設定されてないと使用不可)
     }
   );
 
--- app/src/app/store.ts
+++ app/src/app/store.ts
@@ -1,4 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import counterReducer from '../features/counter/counterSlice';
 import { pokemonApi } from '../services/pokemon';
 
@@ -9,6 +10,9 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(pokemonApi.middleware),
 });
+
+// refetchOnFocusとrefetchOnReconnectを有効にする
+setupListeners(store.dispatch)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
